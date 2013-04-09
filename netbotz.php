d910497a9d3e6e6f8c310716de7f1a2a03b52920
bundy.strand <bundy.strand@gmail.com>
2013-04-09 11:06:38 +0000

    Dummy

--- netbotz.php
+++ netbotz.php
@@ -1,8 +1,12 @@
 <?php
 
 require_once 'config.php';
-function check_sensor($query, $type) {
-	if($dummydata != TRUE) {
+
+function check_sensor($query, $type, $dummydata) {
+	if($dummydata) {
+	 return array(array('name' => 1, 'data' => 12), array('name' => 2, 'data' => 12), array('name' => 4, 'data' => 12), array('name' => 5, 'data' => 12)); 
+	}
+
 	$result = json_decode(file_get_contents($query));
 	$result_obj = $result->result[0];
 	// 'Sensor MM:1 Temperature'=15;;;60;0; 'Sensor MM:2 Temperature'=17;;;60;0; 'Sensor MM:5 Temperature'=22;;;60;0; 'Sensor MM:4 Temperature'=22;;;60;0;
@@ -18,19 +22,16 @@ function check_sensor($query, $type) {
 		array_push($result, $sensor);
 	}
 	return $result;
-	} else {
-	 return array(array('name' => 1, 'data' => 12), array('name' => 2, 'data' => 12), array('name' => 4, 'data' => 12), array('name' => 5, 'data' => 12)); 
-	}
 }
 
 if(isset($_GET['type'])) {
 	$query = $type = null;
 	if($_GET['type'] == 'Temperature') {
 			$query = 'http://' . $APIhost . '/icinga-web/web/api/service/filter[(SERVICE_NAME%7C=%7CCheck%20Netbotz%20Temp)]/countColumn=SERVICE_ID/authkey=' . $APIkey . '/json';
-			echo json_encode(check_sensor($query, 'Temperature'), JSON_NUMERIC_CHECK);
+			echo json_encode(check_sensor($query, 'Temperature', $dummydata), JSON_NUMERIC_CHECK);
 	} 
 	elseif ($_GET['type'] == 'Humidity') {
 		$query = 'http://' . $APIhost . '/icinga-web/web/api/service/filter[(SERVICE_NAME%7C=%7CCheck%20Netbotz%20Humid)]/countColumn=SERVICE_ID/authkey=' . $APIkey . '/json';
-		echo json_encode(check_sensor($query, 'Humidity'), JSON_NUMERIC_CHECK);
+		echo json_encode(check_sensor($query, 'Humidity', $dummydata), JSON_NUMERIC_CHECK);
 	}
 }
